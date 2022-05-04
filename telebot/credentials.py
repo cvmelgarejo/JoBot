6f8e428e071b88558bb305057fcc31b2ba30a253
femellone <fernandomellone2@gmail.com>
2022-05-04 04:25:39 -0400

    test begins

---
+++ telebot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = "place your token here"
+bot_user_name = "bot username"
+URL = "the heroku app link that we will create later"
---
+++ telebot/mastermind.py
@@ -0,0 +1,8 @@
+
+def get_response(msg):
+    """
+    you can place your mastermind AI here
+    could be a very basic simple response like "معلش"
+    or a complex LSTM network that generate appropriate answer
+    """
+    return "معلش !"

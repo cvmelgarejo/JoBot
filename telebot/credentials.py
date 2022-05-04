5b2281b1a52468d7db7ff5e35fb5e73cec38ab66
femellone <fernandomellone2@gmail.com>
2022-05-04 04:29:10 -0400

    adds the credentials values

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "place your token here"
-bot_user_name = "bot username"
-URL = "the heroku app link that we will create later"
+bot_token = "5140624275:AAHSs0vbHKxDt09kF0N69hub6_UlBTKQ32Q"
+bot_user_name = "pengu_jobs_bot"
+URL = "https://test-bot-penguin.herokuapp.com"
